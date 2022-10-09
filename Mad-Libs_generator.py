d847df5fc3423a1b0bb09d17651d6aaeee9135a4
Gautam <gautamwalve@gmail.com>
2022-10-10 00:16:04 +0530

    Mad-Libs Generator Ver 1.0
    Basic Version with Basic Story

---
+++ Mad-Libs_generator.py
@@ -0,0 +1,16 @@
+
+noun = input("Input a Noun: ")
+p_noun = input("Input a Plural Noun: ")
+noun2 = input("Input another Noun: ")
+place = input("Input a Place: ")
+adjective = input("Input a Adjective: ")
+noun3 = input("Last one to go, Input a Noun: ")
+
+#Displays the story based on the user input
+
+print("\n\n\n\nBe Kind to your ",noun,"-footed ", p_noun,
+"\nFor a Duck may be somebody's ",noun2 ,
+"\nBe Kind to your ",p_noun," in ",place,
+"\nWhere the weather is always ",adjective,"."
+"\nYou May think that is this the ",noun3,
+"\nWell it is.")
