4ecc37221f64f7e9e2ba280f51a2ab948e86155f
Gautam Walve <gautamwalve@gmail.com>
2022-11-26 13:11:11 +0530

    updated on 26.November.2022

--- Python Projects/Dice Rolling Simulator/Dice_rolling_Simulator.py
+++ Python Projects/Dice Rolling Simulator/Dice_rolling_Simulator.py
@@ -1,35 +1,17 @@
 import random
-
-x= "y"
-
-while x =="y":    
-#"""#Generate a random number  #between  1 and 6 (including both 1 and 6)"""
-    no = random.randint(1,6)
-    
-    if  no == 1:
-        print("[           ]")
-        print("[     0    ]")
-        print("[           ]")
-    elif no == 2:
-        print("[ 0         ]")
-        print("[            ]")
-        print("[         0 ]")
-    elif no == 3:
-        print("[ 0         ]")
-        print("[     0     ]")
-        print("[         0 ]")
-    elif no == 4:
-        print("[ 0      0 ]")
-        print("[            ]")
-        print("[ 0      0 ]")
-    elif no == 5:
-        print("[ 0      0 ]")
-        print("[     0     ]")
-        print("[ 0      0 ]")
-    elif no == 6:
-        print("[ 0      0 ]")
-        print("[ 0      0 ]")
-        print("[ 0      0 ]")
-    
-    x = input("press y to roll again and n to exit: ")
-    print("\n")
+x = "y"
+while x == "y":
+    x = random.randint(1,6)
+    if x == 1:
+        print("[0 _ _ _ _ _ ]")
+    elif x == 2:
+        print("[0 _ _ _ _ 0 ]")
+    elif x == 3:
+        print("[0 _ 0 _ _ 0 ]")
+    elif x == 4:
+        print("[0 0 _ _ 0 0 ]")
+    elif x == 5:
+        print("[0 0 0 _ 0 0 ]")
+    elif x == 6:
+        print("[0 0 0 0 0 0 ]")
+    x = input("press y to roll again and n to exit:")
